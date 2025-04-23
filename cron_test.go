5f7664ef79399fa1e2053e8b749be35ebd600fc4
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-04-22 22:56:59 -0700

    remove useless options

--- cron_test.go
+++ cron_test.go
@@ -1780,3 +1780,10 @@ func TestSetCleanupInterval(t *testing.T) {
 	cron.SetCleanupInterval(1)
 	cron.SetCleanupInterval(0)
 }
+
+func TestWithLocation(t *testing.T) {
+	c1 := New().Build()
+	assert.Equal(t, time.Local, c1.Location())
+	c2 := New().WithLocation(time.UTC).Build()
+	assert.Equal(t, time.UTC, c2.Location())
+}
